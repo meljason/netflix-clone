349e04b4966eb3e60f7b2298dfe5809a47159eb2
unknown <jasoncwy@hotmail.com>
2020-11-18 12:46:42 -0500

    Structuring styled components homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import Jumbotron from './components/jumbotron/index'
+import jumboData from './fixtures/jumbo';
+import Jumbotron from './components/jumbotron'
 
 export default function App() {
   return (
-    <Jumbotron />
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.Subtitle>{item.subTitle}</Jumbotron.Subtitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
 
