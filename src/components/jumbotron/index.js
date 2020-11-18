2cc09392e602e08f8ca1ce07dea4803bda7d50f6
unknown <jasoncwy@hotmail.com>
2020-11-18 13:38:46 -0500

    Adding footer

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,33 +1,42 @@
-import React from 'react'
-import { Inner, Item, Container, Pane, Image, Subtitle, Title } from './styles/jumbotron';
+import React from "react";
+import {
+  Inner,
+  Item,
+  Container,
+  Pane,
+  Image,
+  Subtitle,
+  Title,
+} from "./styles/jumbotron";
 
-export default function Jumbotron({ children, direction = 'row', ...restProps}) {
-    return (
-        <Item direction={direction}>
-            <Inner>
-                {children}
-            </Inner>
-        </Item>
-    )
+export default function Jumbotron({
+  children,
+  direction = "row",
+  ...restProps
+}) {
+  return (
+    <Item {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  );
 }
 
-Jumbotron.Container = function JumbotronContainer({children, ...restProps}) {
-    return <Container {...restProps}>{children}</Container>
-}
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
 
-Jumbotron.Pane = function JumbotronPane({children, ...restProps}) {
-    return <Pane {...restProps}>{children}</Pane>
-}
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
 
-Jumbotron.Title = function JumbotronTitle({children, ...restProps}) {
-    return <Title {...restProps}>{children}</Title>
-}
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
 
-Jumbotron.Subtitle = function JumbotronSubtitle({children, ...restProps}) {
-    return <Subtitle {...restProps}>{children}</Subtitle>
-}
+Jumbotron.Subtitle = function JumbotronSubtitle({ children, ...restProps }) {
+  return <Subtitle {...restProps}>{children}</Subtitle>;
+};
 
-Jumbotron.Image = function JumbotronImage({...restProps}) {
-    return <Image {...restProps}></Image>
-}
-
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+  return <Image {...restProps}></Image>;
+};
